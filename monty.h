11efe3da6124533808002460254c3db33ab15616
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-06-15 21:49:04 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -6,7 +6,55 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct globalvariables - global struct of variables
+ * @num: value of node
+ * @line: line from getline
+ * @mfile: textfile
+ * @stack: pointer to the head of stack_t
+ * @filename: file name string
+ * @token: token
+ *
+ * Description: variables to share between functions
+ */
+typedef struct globalvariables
+{
+  int num;
+	char *line;
+	FILE *mfile;
+	stack_t *stack;
+	char *filename;
+	char *token;
+} gbv;
 
 extern gbv gv;
 
