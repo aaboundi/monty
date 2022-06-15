7f54791ccddca0a220db3a5af12f459696e3682e
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-06-15 22:00:18 +0000

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,1 +1,17 @@
 #include "monty.h"
+
+/**
+* pall - print all of the values in the stack
+* @stack: pointer to pointer
+* @line_number: unsigned int
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	(void)line_number;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
