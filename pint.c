4ee2bd0fe0d10aa0aca0005777588af171ac6e7a
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-06-16 13:12:34 +0000

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,22 +1,16 @@
 #include "monty.h"
 
 /**
-* push - push num to top of the stack
+* pint - print the first int
 * @stack: pointer to pointer
 * @line_number: unsigned int
 */
-void push(stack_t **stack, unsigned int line_number)
-{
-	stack_t *newNode, *current = *stack;
 
-	(void)line_number;
-	newNode = malloc(sizeof(stack_t));
-	if (!newNode)
-		errorHandler(4, line_number);
-	newNode->n = gv.num;
-	newNode->next = *stack;
-	newNode->prev = NULL;
-	if (*stack)
-		current->prev = newNode;
-	*stack = newNode;
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!*stack)
+		errorHandler(5, line_number);
+	printf("%d\n", temp->n);
 }
