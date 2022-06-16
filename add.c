041c7447e80127655bb34988ecaeb018c812ef66
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-06-16 13:25:57 +0000

    Update add.c

--- add.c
+++ add.c
@@ -1,22 +1,26 @@
 #include "monty.h"
 
 /**
-* push - push num to top of the stack
-* @stack: pointer to pointer
-* @line_number: unsigned int
-*/
-void push(stack_t **stack, unsigned int line_number)
+ * add - a function that implements the add opcode (adds the top two elements
+ * of the stack_t
+ * @stack: a pointer to the head of the stack_t
+ * @line_number: the line number of the opcode from the text file
+ *
+ * Return: void
+ */
+
+void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *newNode, *current = *stack;
+	int sum;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		errorHandler2(17, line_number);
+	(*stack) = (*stack)->next;
+
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 
 	(void)line_number;
-	newNode = malloc(sizeof(stack_t));
-	if (!newNode)
-		errorHandler(4, line_number);
-	newNode->n = gv.num;
-	newNode->next = *stack;
-	newNode->prev = NULL;
-	if (*stack)
-		current->prev = newNode;
-	*stack = newNode;
 }
